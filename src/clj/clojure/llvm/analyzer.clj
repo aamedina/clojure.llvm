8116e721109c60c82c47d46e1470325f4948b2d9
Adrian Medina <adrian.medina@mail.yu.edu>
2014-03-09 21:41:15 -0400

    define basic create-var fn

--- src/clj/clojure/llvm/analyzer.clj
+++ src/clj/clojure/llvm/analyzer.clj
@@ -1,1 +1,37 @@
-(ns clojure.llvm.analyzer)
+(ns clojure.llvm.analyzer
+  (:require [clojure.tools.analyzer :as ana]
+            [clojure.tools.analyzer.passes
+             [source-info :refer [source-info]]
+             [cleanup :refer [cleanup]]
+             [elide-meta :refer [elide-meta]]
+             [warn-earmuff :refer [warn-earmuff]]
+             [collect :refer [collect collect-closed-overs]]
+             [add-binding-atom :refer [add-binding-atom]]
+             [uniquify :refer [uniquify-locals]]]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.set :as set]
+            [clojure.llvm.env :as env]
+            [clojure.tools.reader :as reader]
+            [clojure.tools.reader.reader-types :as reader-types])
+  (:import java.lang.StringBuilder
+           java.io.File))
+
+(set! *warn-on-reflection* true)
+
+(def vars (atom {}))
+
+(def specials
+  (into ana/specials '#{}))
+
+(defn create-var
+  [sym {:keys [ns]}]
+  (get (swap! vars assoc sym nil) sym))
+
+(defn analyze
+  [form env]
+  (binding [ana/macroexpand-1 macroexpand-1
+            ana/create-var create-var
+            ana/parse ana/-parse
+            ana/var? (fn [var] (contains? @vars var))]
+    (ana/analyze form env)))
