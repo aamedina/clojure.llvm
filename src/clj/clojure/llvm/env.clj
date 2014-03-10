818ba0e61f8edaaa219f09be502da4d25358d99d
Adrian Medina <adrian.medina@mail.yu.edu>
2014-03-09 21:21:26 -0400

    model env ns after cljs.env

--- src/clj/clojure/llvm/env.clj
+++ src/clj/clojure/llvm/env.clj
@@ -2,3 +2,30 @@
   (:refer-clojure :exclude [ensure]))
 
 (def ^:dynamic *compiler* nil)
+
+(defn default-compiler-env
+  ([] (default-compiler-env {}))
+  ([options] (atom {:options options})))
+
+(defmacro with-compiler-env
+  [env & body]
+  `(let [env# ~env
+         env# (cond
+                (map? env#) (atom env#)
+                (and (instance? clojure.lang.Atom env#) (map? @env#)) env#
+                :else (throw (IllegalArgumentException.
+                              (str "Compiler environment must be a map or atom"
+                                   " containing a map, not " (class env#)))))]
+     (binding [*compiler* env#] ~@body)))
+
+(defmacro ensure
+  [& body]
+  `(let [val# *compiler*]
+     (if (nil? val#)
+       (push-thread-bindings
+        (hash-map (var *compiler*) (default-compiler-env)))
+       (try
+         ~@body
+         (finally
+           (when (nil? val#)
+             (pop-thread-bindings)))))))
